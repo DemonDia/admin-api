0a21289c209554468547c070f74d7b434ec10cbd
Setsuna Yukiii <siang-meng@live.com>
2022-12-02 23:01:21 +0800

    adding of project model

---
+++ models/projectModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+const contactSchema = mongoose.Schema({
+    projectname:{
+        // project name
+        type:String,
+        required:[true,"Please add a name"]
+    },
+    year:{
+        // pubished year
+        type:String,
+        required:[true,"Please add a year"]
+    },
+    description:{
+        // string array
+        type:Array,
+        required:[true,"Please add some description"]
+    },
+    techstacks:{
+        // string array
+        type:Array,
+        required:[true,"Please add some tech stacks"]
+    },
+    links:{
+        // Object array with the following properties:
+        // LinkName
+        // LinkURL
+        type:Array,
+        required:[true,"Please add some links"]
+    },
+    components:{
+        // Object array with the following properties
+        // ComponentName
+        // ComponentLink
+        type:Array,
+        required:[true,"Please add some components if any"]
+    }
+})
