bb1a5df59fb25b55b812f6285b776a0f0b3801f4
Setsuna Yukiii <siang-meng@live.com>
2022-12-02 20:24:29 +0800

    adding of contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,19 +4,19 @@ const User = require("../models/userModel");
 // ========================get all contacts on db========================
 const getAllContacts = async (req, res) => {
     await Contact.find()
-    .then((result) => {
-        res.send({
-            success: true,
-            data: result,
+        .then((result) => {
+            res.send({
+                success: true,
+                data: result,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send({
+                success: false,
+                message: err,
+            });
         });
-    })
-    .catch((err) => {
-        console.log(err);
-        res.send({
-            success: false,
-            message: err,
-        });
-    });
 };
 // ========================get all user contacts on db========================
 const getUserContacts = async (req, res) => {
@@ -51,7 +51,44 @@ const getUserContacts = async (req, res) => {
     }
 };
 // ========================add contact to db========================
-const addContact = async (req, res) => {};
+const addContact = async (req, res) => {
+    const userId = req.body.userId;
+    if (userId.length != 24) {
+        res.send({
+            success: false,
+            message: "User does not exist",
+        });
+    } else {
+        const getUser = await User.findOne({ _id: userId });
+        if (!getUser) {
+            res.send({
+                success: false,
+                message: "User does not exist",
+            });
+        } else {
+            const newContact = new Contact({
+                userId,
+                contactname: req.body.contactname,
+                contact: req.body.contactinfo,
+            });
+
+            await Contact.create(newContact)
+                .then((result) => {
+                    res.send({
+                        success: true,
+                        message: "Contact added",
+                        data: newContact._id,
+                    });
+                })
+                .catch((err) => {
+                    res.send({
+                        success: false,
+                        message: err,
+                    });
+                });
+        }
+    }
+};
 // ========================update specific contact to db========================
 const updateContact = async (req, res) => {};
 // ========================delect specific contact from db========================
