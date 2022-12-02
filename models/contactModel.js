eaa453e9ddf0af2e1f9388ca5e09f44310087b81
Setsuna Yukiii <siang-meng@live.com>
2022-12-02 20:00:54 +0800

    add contact schema

---
+++ models/contactModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const contactSchema = mongoose.Schema({
+    contactname:{
+        type:String,
+        required:[true,"Please add a name"]
+    },
+    contact:{
+        type:String,
+        required:[true,"Please add a link/tag/email"]
+    },
+    userId:{
+        type:String,
+        required:[true,"Please add a user"]
+    }
+})
+
+module.exports = mongoose.model("Contact",contactSchema)
