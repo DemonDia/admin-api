030c155fe436783728b14597d1eaf549af060a31
Setsuna Yukiii <siang-meng@live.com>
2022-12-02 23:24:03 +0800

    add project & get project by its ID

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -51,9 +51,79 @@ const getUserProjects = async (req, res) => {
     }
 };
 // ========================add a project to db========================
-const addProject = async (req, res) => {};
+const addProject = async (req, res) => {
+    const userId = req.body.userId;
+    if (userId.length != 24) {
+        res.send({
+            success: false,
+            message: "User does not exist",
+        });
+    } else {
+        const getUser = await User.findOne({ _id: userId });
+        if (!getUser) {
+            res.send({
+                success: false,
+                message: "User does not exist",
+            });
+        } else {
+            const newProject = new Project({
+                projectname: req.body.projectname,
+                year: req.body.year,
+                description: req.body.description,
+                techstacks: req.body.techstacks,
+                links: req.body.links,
+                components: req.body.components,
+                userId,
+            });
+
+            await Project.create(newProject)
+                .then((result) => {
+                    res.send({
+                        success: true,
+                        message: "Project added",
+                        data: newProject._id,
+                    });
+                })
+                .catch((err) => {
+                    res.send({
+                        success: false,
+                        message: err,
+                    });
+                });
+        }
+    }
+};
 // ========================get project by ID========================
-const getProjectById = async (req, res) => {};
+const getProjectById = async (req, res) => {
+    const projectId = req.params.projectId;
+    if (projectId.length != 24) {
+        res.send({
+            success: false,
+            message: "Project is not found",
+        });
+    } else {
+        await Project.findById(projectId)
+            .then((result) => {
+                if (!result) {
+                    res.send({
+                        success: false,
+                        message: "Project is not found",
+                    });
+                } else {
+                    res.send({
+                        success: true,
+                        data: result,
+                    });
+                }
+            })
+            .catch((err) => {
+                res.send({
+                    success: false,
+                    message: err,
+                });
+            });
+    }
+};
 // ========================update specific project on db========================
 const updateProject = async (req, res) => {};
 // ========================delete a project from db========================
