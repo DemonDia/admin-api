698ab6a7faf6a4bf86be81b9559171e9837cb405
Setsuna Yukiii <siang-meng@live.com>
2022-12-02 20:18:02 +0800

    get all contacts function

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,7 +2,22 @@ const Contact = require("../models/contactModel");
 const User = require("../models/userModel");
 
 // ========================get all contacts on db========================
-const getAllContacts = async (req, res) => {};
+const getAllContacts = async (req, res) => {
+    await Contact.find()
+    .then((result) => {
+        res.send({
+            success: true,
+            data: result,
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send({
+            success: false,
+            message: err,
+        });
+    });
+};
 // ========================get all user contacts on db========================
 const getUserContacts = async (req, res) => {};
 // ========================add contact to db========================
