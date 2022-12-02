5c4239b368ad1ace651ae36e3881fd2165898153
Setsuna Yukiii <siang-meng@live.com>
2022-12-02 20:06:00 +0800

    add skeletons for methods in contact controller

---
+++ controllers/contactController.js
@@ -0,0 +1,21 @@
+const Contact = require("../models/contactModel");
+const User = require("../models/userModel");
+
+// ========================get all contacts on db========================
+const getAllContacts = async (req, res) => {};
+// ========================get all user contacts on db========================
+const getUserContacts = async (req, res) => {};
+// ========================add contact to db========================
+const addContact = async (req, res) => {};
+// ========================update specific contact to db========================
+const updateContact = async (req, res) => {};
+// ========================delect specific contact from db========================
+const deleteContact = async (req, res) => {};
+
+module.exports = {
+    getAllContacts,
+    getUserContacts,
+    addContact,
+    updateContact,
+    deleteContact,
+};
